cf0533e376a499da3730a32f810980645a83f178
simon <simon.hobbs@electrooptical.net>
2021-11-19 23:14:34 -0500

    adding file checks on clones

--- setup_embedded.sh
+++ setup_embedded.sh
@@ -2,32 +2,49 @@
 
 export project_name=$1
 
-touch README.md
-wget https://raw.githubusercontent.com/snhobbs/CMakeGitignore/main/.gitignore
-# curl default top level cmake
+if test ! -f "README.md" ; then
+  touch README.md
+  echo "Project: $project_name" >> README.md
+fi
 
-mkdir board
-mkdir board/include
-mkdir board/source
+if test ! -f "CMakeLists.txt" ; then
+  https://raw.githubusercontent.com/snhobbs/EmbeddedProjectSetup/master/CMakeLists.txt
+fi
 
-mkdir project
-mkdir project/source
+if test ! -f ".gitignore" ; then
+  wget https://raw.githubusercontent.com/snhobbs/CMakeGitignore/main/.gitignore
+fi
+
+mkdir -p board/{include/source}
+
+mkdir -p project/source
 # curl default project cmake
 # default main.cpp?
 
-mkdir tests
+mkdir -p tests/source
 touch tests/README.md
-mkdir tests/source
-# curl default test cmake
+cd tests
+if test ! -f "CMakeLists.txt" ; then
+  wget https://raw.githubusercontent.com/snhobbs/EmbeddedProjectSetup/master/tests/CMakeLists.txt
+fi
+
+cd source
+
+if test ! -f "main.cpp" ; then
+  wget https://raw.githubusercontent.com/snhobbs/EmbeddedProjectSetup/master/tests/source/main.cpp
+fi
+
+cd ../.. # top
 
 # Setup Toolchain
 mkdir toolchains
 cd toolchains
-export toolchain=gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
-if test ! -f "$toolchain" ; then
-  wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/$toolchain
+export toolchain=gcc-arm-none-eabi-10.3-2021.10
+echo "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/$toolchain-x86_64-linux.tar.bz2"
+if test ! -d "$toolchain" ; then
+  wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/$toolchain-x86_64-linux.tar.bz2"
+  tar -xf "$toolchain"-x86_64-linux.tar.bz2
 fi
-tar -xf $toolchain
 cd ../ # top
 
 mkdir emulator
@@ -36,9 +53,8 @@ mkdir documents
 touch documents/design.md
 
 mkdir libs
-# get gtest & gmock, link into tests
-# get default main.cpp for testing
 cd libs
+# get gtest & gmock, link into tests
 if test ! -f "googletest" ; then
   git clone git@github.com:google/googletest.git
 fi
