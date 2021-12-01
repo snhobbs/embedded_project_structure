806cf7d1bed982ca8888b625deca5bd5385ac16b
simon <simon.hobbs@electrooptical.net>
2021-12-01 13:27:46 -0500

    adding submodules

--- setup_embedded.sh
+++ setup_embedded.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+git init .
 export project_name=$1
+REPO_NAME="https://raw.githubusercontent.com/snhobbs/embedded_project_structure/master/"
 
 if test ! -f "README.md" ; then
   touch README.md
-  echo "Project: $project_name" >> README.md
+  echo "#Project: $project_name" >> README.md
 fi
 
 if test ! -f "CMakeLists.txt" ; then
-  https://raw.githubusercontent.com/snhobbs/EmbeddedProjectSetup/master/CMakeLists.txt
+  wget $REPO_NAME"CMakeLists.txt"
 fi
 
 if test ! -f ".gitignore" ; then
-  wget https://raw.githubusercontent.com/snhobbs/CMakeGitignore/main/.gitignore
+  wget $REPO_NAME"embedded_project.gitignore" .gitignore
 fi
 
-mkdir -p board/{include/source}
+mkdir -p board/{include,source}
 
 mkdir -p project/source
 # curl default project cmake
@@ -25,13 +27,15 @@ mkdir -p tests/source
 touch tests/README.md
 cd tests
 if test ! -f "CMakeLists.txt" ; then
-  wget https://raw.githubusercontent.com/snhobbs/EmbeddedProjectSetup/master/tests/CMakeLists.txt
+  wget $REPO_NAME"tests/CMakeLists.txt"
 fi
+
+ln -s ../libs/googletest ./
 
 cd source
 
 if test ! -f "main.cpp" ; then
-  wget https://raw.githubusercontent.com/snhobbs/EmbeddedProjectSetup/master/tests/source/main.cpp
+  wget $REPO_NAME"tests/source/main.cpp"
 fi
 
 cd ../.. # top
@@ -39,15 +43,18 @@ cd ../.. # top
 # Setup Toolchain
 mkdir toolchains
 cd toolchains
-export toolchain=gcc-arm-none-eabi-10.3-2021.10
-echo "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/$toolchain-x86_64-linux.tar.bz2"
-if test ! -d "$toolchain" ; then
-  wget "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/$toolchain-x86_64-linux.tar.bz2"
-  tar -xf "$toolchain"-x86_64-linux.tar.bz2
+export TOOLCHAIN="10.3-2021.10"
+export TOOLCHAIN_DIR="gcc-arm-none-eabi-"$TOOLCHAIN
+export TOOLCHAIN_TARBALL=$TOOLCHAIN_DIR"-x86_64-linux.tar.bz2"
+export TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/"$TOOLCHAIN"/"$TOOLCHAIN_TARBALL
+echo $TOOLCHAIN_URL
+echo $TOOLCHAIN_DIR
+
+if test ! -d "$TOOLCHAIN_DIR" ; then
+  wget $TOOLCHAIN_URL
+  tar -xf $TOOLCHAIN_TARBALL
 fi
 cd ../ # top
-
-mkdir emulator
 
 mkdir documents
 touch documents/design.md
@@ -56,17 +63,13 @@ mkdir libs
 cd libs
 # get gtest & gmock, link into tests
 if test ! -f "googletest" ; then
-  git clone git@github.com:google/googletest.git
+  git submodule add git@github.com:google/googletest.git
 fi
 
 cd googletest
-cmake ./
-make -j4
+cmake ./ -B build
+make -j4 --directory=build
 cd ../ # libs
-
-if test ! -f "CMakeTools" ; then
-  git clone git@github.com:snhobbs/CMakeTools.git
-fi
 cd ../ # top
 
 mkdir releases
